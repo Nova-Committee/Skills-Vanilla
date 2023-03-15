93feb879935b312cba7d27cb0ff62ce07d530b89
MikhailTapio <mikhailtapio13468@163.com>
2023-03-15 17:52:19 +0800

    Stealth adjustment

--- src/main/scala/committee/nova/skillsvanilla/implicits/Implicits.scala
+++ src/main/scala/committee/nova/skillsvanilla/implicits/Implicits.scala
@@ -12,12 +12,18 @@ object Implicits {
       if (!player.isSneaking || player.world.getWorldTime % 20 != 0) return 0
       val mobs = player.world.getEntitiesWithinAABB(classOf[EntityMob], new AxisAlignedBB(player.posX - 16.0, player.posY - 16.0, player.posZ - 16.0, player.posX + 16.0, player.posY + 16.0, player.posZ + 16.0))
       var freeMob = false
+      var isSeen = false
+      var targeted = false
       mobs.asScala.foreach(m => {
         val target = m.getAttackTarget
-        if (m.canEntityBeSeen(player) && target == null) freeMob = true
-        if (target == player) return 1
+        val seen = m.canEntityBeSeen(player)
+        if (seen) {
+          if (target == null) freeMob = true
+          if (target == player) targeted = true
+          isSeen = true
+        }
       })
-      if (freeMob) 2 else 1
+      if (freeMob && !targeted) 2 else if (isSeen) 1 else 0
     }
   }
 }
