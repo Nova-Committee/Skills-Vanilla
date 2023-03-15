801eea788ab256958a133c453dcd067476f4c3cc
MikhailTapio <mikhailtapio13468@163.com>
2023-03-15 18:29:20 +0800

    Swimming adjustment

--- src/main/scala/committee/nova/skillsvanilla/event/handler/FMLEventHandler.scala
+++ src/main/scala/committee/nova/skillsvanilla/event/handler/FMLEventHandler.scala
@@ -3,10 +3,13 @@ package committee.nova.skillsvanilla.event.handler
 import committee.nova.skillful.implicits.Implicits.EntityPlayerImplicit
 import committee.nova.skillsvanilla.implicits.Implicits.PlayerImplicit
 import committee.nova.skillsvanilla.registries.VanillaSkills
+import committee.nova.skillsvanilla.registries.VanillaSkills.SWIMMING
 import net.minecraft.entity.player.EntityPlayerMP
 import net.minecraftforge.fml.common.FMLCommonHandler
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent.{Phase, PlayerTickEvent}
+
+import scala.util.Random
 
 object FMLEventHandler {
   def init(): Unit = FMLCommonHandler.instance().bus().register(new FMLEventHandler)
@@ -22,5 +25,8 @@ class FMLEventHandler {
     // Will related exhaustion
     p.addExhaustion(-0.001F * p.getSkillStat(VanillaSkills.WILL).getCurrentLevel max -p.getFoodStats.foodExhaustionLevel)
     p.getSkillStat(VanillaSkills.STEALTH).addXp(p, p.getStealthEffect)
+    val air = p.getAir
+    val airPercent = air / 300.0
+    if ((p.getSkillStat(SWIMMING).getCurrentLevel / 200.0) * (1.0 - airPercent) > new Random().nextDouble()) p.setAir((air + 1) max 300)
   }
 }
